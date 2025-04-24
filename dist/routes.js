ed0dc2213736f147dbd7ee385eb536c49875cdea
Rael Martins <77245288+RaelMartinss@users.noreply.github.com>
2025-04-24 18:20:17 -0300

    cre

---
+++ dist/routes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get('/usuarios', async (req, res) => {
+    const usuarios = await prisma.usuario.findMany();
+    res.json(usuarios);
+});
